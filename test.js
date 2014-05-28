405554eabf40ff124d67fe02b6ca8dd18ecb812c
Michael van der Weg <michael@joinbox.com>
2014-05-28 19:26:49 +0200

    added package versions

--- test.js
+++ test.js
@@ -8,12 +8,28 @@
 		on: {
 			  error: function( err ){ log.trace( err ); }
 			, log: function( group, id, status, data ){ log.debug( group, id, status, data ); }
-			, task: function( group, id, next ){ log.info( group, id ); setTimeout( next , 2000 ); }
 		}
 	} );
 
 
 
-	s.schedule( [ "1", "fri" ], [ "03:00", "1:1" ] );
-	s.interval( "1s", null, "testee", 5 );
-	s.interval( "1s", null, "another", 5 );
+	//s.schedule( [ "1", "fri" ], [ "03:00", "1:1" ] );
+	var x = new Date();
+	x.setHours( 3 );
+	x.setMinutes( 0 );
+	
+
+	log(s.interval( "1s", x, "testee", 5 ));
+	//s.interval( "1s", null, "another", 5 );
+
+
+	s.interval( "1s", x, "import", 5 )
+	s.interval( "1 day", x, "import", 5 )
+
+	s.on('task', function(group, id, next){
+		if (group === 'import'){
+			//s.import(next);
+		}
+		 log.info( group, id ); 
+		 setTimeout( next , 2000 );
+	});
